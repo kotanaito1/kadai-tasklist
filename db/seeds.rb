9bdc3d21c68717d4db9dc3c08d88027cdd24cb26
kota naito <kn.ts.cb@gmail.com>
2017-06-14 14:28:15 +0000

    for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Task.create(content:'test content 1', status: 'test status 1')
-Task.create(content:'test content 2', status: 'test status 2')
-Task.create(content:'test content 3', status: 'test status 3')
+Task.create(content:'test content 1', status: 'test1')
+Task.create(content:'test content 2', status: 'test2')
+Task.create(content:'test content 3', status: 'test3')
